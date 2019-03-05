dd69fca0f979a9c172eb6197e8db4b96b421d683
anusharshankar <anusha2089@gmail.com>
2019-03-05 10:35:41 -0800

    Adding LDAP services

---
+++ PolicyLDAP/Identity/ILdapEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolicyLDAP.Identity
+{
+    interface ILdapEntry
+    {
+        string ObjectSid { get; set; }
+        string ObjectGuid { get; set; }
+        string ObjectCategory { get; set; }
+        string ObjectClass { get; set; }
+        string Name { get; set; }
+        string CommonName { get; set; }
+        string DistinguishedName { get; set; }
+        int SamAccountType { get; set; }
+    }
+}
